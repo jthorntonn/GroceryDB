ad8b7913cf310b4874a4507c085056a84be8aef7
Florent Dondjeu Tschoufack <florentdondjeu@yahoo.com>
2021-04-07 19:48:19 -0400

    Added dropdown to see products of other stores

--- index.php
+++ index.php
@@ -36,7 +36,10 @@
 		<div class="header-middle">
 			<h1>STORE.COM</h1>
 			<p id="addr">Store address goes here...</p>
-			<p id="storeNumb">Store #</p>
+			<select id="storeNumb">
+				<option value="Store1" selected>Store 1</option>
+				<option value="Store2">Store 2</option> 
+			</select>
 		</div>
 		<div class="header-login">
 			<button type="button" id="loginbutton">Manager<br>Login</button>
@@ -54,12 +57,24 @@
 		</form>
 	</div>
 
-	<div class="gallery-slider">
-		<div class="gallery-container">	
-			<!-- script fills class with product from db --!>
-			<script src="js/foodData.js"> </script>
+	<div class="gallery-slider" style="float:left;">
+
+		<div class="gallery-container" style="float:left;">	
 		</div>
 		
+			<script>
+				for(let i=0; i < food_array.length; i++) {
+					if( food_array[i][0] == $("#storeNumb").val()) {
+						$("#addr").html(food_array[i][2]);
+						$(".gallery-container" ).append(
+							'<div class="item"> <div class="foodinfo" >'  +
+								food_array[i][3] + '<br>' +
+								food_array[i][4] + '<br>$' +
+								food_array[i][5] +
+								'</div></div>');
+					}	
+				}
+			</script>
 		<div class="gallery-controls">
 			<ul>
 				<li id="<"><</li>
--- js/galleryScript.js
+++ js/galleryScript.js
@@ -10,11 +10,30 @@ $(document).ready(function () {
 			numbItemsDisplaped = sizes[i].breakPoint.item;
 		}
 	}
-	/* style */
+
 	var n = $(".gallery-container > .item").length;  // number of '.items'
 	const CW = $(".gallery-container").width();  // container width
 	const M = 15;  // margin
 
+	$("#storeNumb").trigger("change");
+	$("#storeNumb").change(function() {
+		$(".gallery-container").empty();
+		for(let i=0; i < food_array.length; i++) {
+			if( food_array[i][0] == $("#storeNumb").val()) {
+				$("#addr").html(food_array[i][2]);
+				$(".gallery-container" ).append(
+				'<div class="item"> <div class="foodinfo" >'  +
+					food_array[i][3] + '<br>' +
+					food_array[i][4] + '<br>$' +
+					food_array[i][5] +
+				'</div></div>');
+			}
+		}
+		$(".gallery-container > .item").css("width", (CW/numbItemsDisplaped)-M + "px" );
+		$(".gallery-container > .item").css("margin", (M/2) +"px");
+	});
+
+	/* Style  */
 	$(".gallery-container > .item").css("width", (CW/numbItemsDisplaped)-M + "px" );
 	$(".gallery-container > .item").css("margin", (M/2) +"px");
 	$(".gallery-container").css("width", (CW/numbItemsDisplaped)*n + "px");
@@ -40,4 +59,5 @@ $(document).ready(function () {
 		console.log("jump: "+jump);
 		console.log((CW / numbItemsDisplaped) * n);
 	});
+
 });
