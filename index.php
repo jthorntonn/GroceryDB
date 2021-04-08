6287d883c7c798417b94e0493e3bc1b114997247
Florent Dondjeu Tschoufack <florentdondjeu@yahoo.com>
2021-04-08 13:55:43 -0400

    add functions.php for connections & clean code

--- index.php
+++ index.php
@@ -11,22 +11,23 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<script src="js/galleryScript.js"></script>
 	<?php
-		if($connection=@mysqli_connect('localhost','fdondjeutschoufack1','fdondjeutschoufack1','GroceryDB')) {
-			$query="SELECT * FROM Sells NATURAL JOIN Product NATURAL JOIN Store";
-			$t =mysqli_query($connection, $query);
-			$food_array = array();
-			while($row=mysqli_fetch_array($t)) {
-				//a double array of each stores with relevant data
-				array_push($food_array, 
-					array($row['name'],$row['phoneNum'],$row['addr'],$row['pName'],$row['description'],$row['sellPrice']) );
-			}
-			// turn php array into js array
-			echo '<script> var food_array = ' . json_encode($food_array) . '; </script>' ;
-			mysqli_close($connection);
-		}
-		else{
-			echo 'not connected to DB!';
-		}	
+	include 'functions.php';
+	$connection = connect();
+	if (!$connection) {
+		//die prints message then ends script
+		die("Connection failed: " . $connection->connect_error);
+	}
+
+	$query="SELECT * FROM Sells NATURAL JOIN Product NATURAL JOIN Store";
+	$t =mysqli_query($connection, $query);
+	$food_array = array();
+	while($row=mysqli_fetch_array($t)) {
+		//a double array of each stores with relevant data
+		array_push($food_array, array($row['name'],$row['phoneNum'],$row['addr'],$row['pName'],$row['description'],$row['sellPrice']) );
+	}
+	// turn php array into js array
+	echo '<script> var food_array = ' . json_encode($food_array) . '; </script>' ;
+	close($connection);
 	?>	
 </head>
 <body>
@@ -56,24 +57,20 @@
 	</div>
 
 	<div class="gallery-slider" style="float:left;">
-
-		<div class="gallery-container" style="float:left;">	
-		</div>
-		
-			<script>
-				/* Insert data from DB into html to be styled inta a gallery view  */
-				for(let i=0; i < food_array.length; i++) {
-					if( food_array[i][0] == $("#storeNumb").val()) {
-						$("#addr").html(food_array[i][2]);
-						$(".gallery-container" ).append(
-							'<div class="item"> <div class="foodinfo" >'  +
-								food_array[i][3] + '<br>' +
-								food_array[i][4] + '<br>$' +
-								food_array[i][5] +
-								'</div></div>');
-					}	
-				}
-			</script>
+		<div class="gallery-container" style="float:left;"> </div>
+		<script>
+		/* Insert data from DB into html to be styled inta a gallery view  */
+		for(let i=0; i < food_array.length; i++) {
+			if( food_array[i][0] == $("#storeNumb").val()) {
+				$("#addr").html(food_array[i][2]);
+				$(".gallery-container" ).append(
+					"<div class=\"item\"> <div class=\"foodinfo\" >" +
+					food_array[i][3] + "<br>" +
+					food_array[i][4] + "<br>$" +
+					food_array[i][5] + "</div></div>");
+				}	
+			}
+		</script>
 		<div class="gallery-controls">
 			<ul>
 				<li id="<"><</li>
@@ -81,7 +78,5 @@
 			</ul>
 		</div>
 	</div>
-
 </body>
 </html>
-
--- js/galleryScript.js
+++ js/galleryScript.js
@@ -16,25 +16,6 @@ $(document).ready(function () {
 	const CW = $(".gallery-container").width();  // container width
 	const M = 15;  // margin
 
-	// event function for when the value in store dropdown is changed
-	$("#storeNumb").change(function() {
-		$(".gallery-container").empty();
-		for(let i=0; i < food_array.length; i++) {
-			if( food_array[i][0] == $("#storeNumb").val()) {
-				$("#addr").html(food_array[i][2]);
-				$(".gallery-container" ).append(
-				'<div class="item"> <div class="foodinfo" >'  +
-					food_array[i][3] + '<br>' +
-					food_array[i][4] + '<br>$' +
-					food_array[i][5] +
-				'</div></div>');
-			}
-		}
-		// apply styling so that the width of each item stay consistant
-		$(".gallery-container > .item").css("width", (CW/numbItemsDisplaped)-M + "px" );
-		$(".gallery-container > .item").css("margin", (M/2) +"px");
-	});
-
 	/* Style  */
 	$(".gallery-container > .item").css("width", (CW/numbItemsDisplaped)-M + "px" );
 	$(".gallery-container > .item").css("margin", (M/2) +"px");
@@ -54,14 +35,33 @@ $(document).ready(function () {
 			}
 		}
 		if (target.attr("id") == ">") {
-			// 							(total width - width of one item)
-			if (jump < ((CW / numbItemsDisplaped) * n) - (CW / numbItemsDisplaped)*2 ) {
+			//(total width - width of item displayed item)
+			if (jump < ((CW / numbItemsDisplaped) * n) - (CW / numbItemsDisplaped)*numbItemsDisplaped ) {
 				jump = jump + (CW);
 				$(".gallery-container").css("margin-left", -jump + "px");
 			}
 		}
 		//console.log("jump: "+jump);
 		//console.log((CW / numbItemsDisplaped) * n);
+	});
+
+	// event function for when the value in store dropdown is changed
+	$("#storeNumb").change(function () {
+		$(".gallery-container").empty();
+		for (let i = 0; i < food_array.length; i++) {
+			if (food_array[i][0] == $("#storeNumb").val()) {
+				$("#addr").html(food_array[i][2]);
+				$(".gallery-container").append(
+					'<div class="item"> <div class="foodinfo" >' +
+					food_array[i][3] + '<br>' +
+					food_array[i][4] + '<br>$' +
+					food_array[i][5] +
+					'</div></div>');
+			}
+		}
+		// apply styling so that the width of each item stay consistant
+		$(".gallery-container > .item").css("width", (CW / numbItemsDisplaped) - M + "px");
+		$(".gallery-container > .item").css("margin", (M / 2) + "px");
 	});
 
 });
